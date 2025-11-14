616977e6fbd4a11652ab6fee4e5362c697f95637
Illia <illiaholodniak@gmail.com>
2025-11-14 14:14:24 +0100

    Add .env.example and update config to use env vars
    Introduced a .env.example file to document required environment variables for mail and Telegram bot configuration. Updated src/config.py to load all configuration values from environment variables, improving flexibility and security.

--- src/config.py
+++ src/config.py
@@ -5,14 +5,14 @@ load_dotenv()
 
 class Config:
     # imap configuration
-    IMAP_SERVER = 'imap.gmail.com'
-    IMAP_PORT = 993
-    EMAIL_ADDRESS = getenv("GMAIL_EMAIL")
-    APP_PASSWORD = getenv('GMAIL_APP_PASSWORD')
+    IMAP_SERVER = getenv('IMAP_SERVER')
+    IMAP_PORT = int(getenv('IMAP_PORT', 993))
+    EMAIL_ADDRESS = getenv('EMAIL_ADDRESS')
+    APP_PASSWORD = getenv('EMAIL_APP_PASSWORD')
 
     # telegram bot configuration
     BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN')
     CHAT_ID = getenv('TELEGRAM_CHAT_ID')
 
     # email check interval in seconds
-    CHECK_INTERVAL = 60
+    CHECK_INTERVAL = int(getenv('CHECK_INTERVAL', 60))
